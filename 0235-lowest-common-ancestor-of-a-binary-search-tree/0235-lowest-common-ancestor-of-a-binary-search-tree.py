64c8633f13ce9b28abd5e31982e3e66665a1bf70
parvas662 <59643791+parvas662@users.noreply.github.com>
2023-12-15 12:13:15 +0530

    Time: 75 ms (45.82%), Space: 20.9 MB (44.90%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,27 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # optimal code
         def solver(root,p,q):
-            lbool = False
-            rbool = False
             if root == None:
-                return None , False     
-            if root.val == p.val or root.val == q.val:
-                return root , True
-            elif root.val > p.val and root.val > q.val:
-                lft ,lbool = solver(root.left,p,q)
+                return None     
+            if root.val > p.val and root.val > q.val:
+                return solver(root.left,p,q)
             elif root.val < p.val and root.val < q.val:
-                rgt ,rbool = solver(root.right,p,q)
-            else:
-                lft ,lbool = solver(root.left,p,q)
-                rgt ,rbool = solver(root.right,p,q)
-            if lbool == True and rbool == True:
-                return root , True
-            if lbool == True:
-                return lft , True
-            if rbool == True:
-                return rgt , True
-            return None , False
+                return solver(root.right,p,q) 
+            return root 
 
-        ans , boolean = solver(root,p,q)
-        return ans
+        return solver(root,p,q)
